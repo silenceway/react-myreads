ddb6e7548ab768402a468209b6427de6f0cf6b9c
Byron H <silenceway@hotmail.com>
2017-10-15 20:32:02 -0500

    Adding book api and creating book item.

--- src/App.js
+++ src/App.js
@@ -4,16 +4,39 @@ import logo from './logo.svg';
 import ListBooks from './ListBooks';
 import SearchBook from './SearchBook';
 import './App.css';
+import * as BooksAPI from './BooksAPI';
 
 class App extends Component {
+  state = {
+    books: []
+  };
+
+  getAllBooks = () => {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books });
+    });
+  };
+
+  updateBook = (book) => {
+    BooksAPI.update(book, "currentlyReading").then((booksStatus) => {
+      this.getAllBooks();
+    });
+  };
+
+  componentDidMount () {
+    this.getAllBooks();
+  }
+
   render() {
     return (
       <div className="app">
-        <Route path="/" exact 
+        <Route path="/" exact
           render={() => (
-            <ListBooks />
+            <ListBooks
+              books={this.state.books}
+              onUpdateBook="" />
           )} />
-        <Route path="/search" 
+        <Route path="/search"
           render={({ history }) => (
             <SearchBook />
           )} />
---
+++ src/Book.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class Book extends Component {
+
+  render () {
+    const book = this.props.book;
+
+    return (
+      <li>
+        <div className="book">
+          <div className="book-top">
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("' + book.imageLinks.thumbnail + ')' }}></div>
+            <div className="book-shelf-changer">
+              <select value={book.shelf}>
+                <option value="none" disabled>Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+              </select>
+            </div>
+          </div>
+          <div className="book-title">{book.title}</div>
+          <div className="book-authors">
+            {book.authors.map((author, i) => <span>
+                {!!i && ", "}
+                {author}
+                </span>
+            )}
+          </div>
+        </div>
+      </li>
+    )
+  }
+}
+
+export default Book
